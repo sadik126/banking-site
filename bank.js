90734c0fc6ec063f46292595152c5f862be0733c
sadik126 <sadikhimel04@gmail.com>
2022-02-10 19:39:22 +0600

    dashboard

---
+++ bank.js
@@ -0,0 +1,12 @@
+document.getElementById('loginbtn').addEventListener('click',function(){
+    let emailfield = document.getElementById('email');
+    let emailvalue = emailfield.value;
+    
+    //get password
+    let passfield = document.getElementById('password')
+    let passvalue = passfield.value;
+    
+    if(emailvalue == 'sadikhimel04@gmail.com' && passvalue == '1234'){
+        window.location.href ='dashboard.html';
+    }
+})
